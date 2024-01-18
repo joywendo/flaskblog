2a113ffa1fd55e2e98dafa0ef435291334afda1f
joywendo <wendokingoo@gmail.com>
2024-01-18 12:42:40 +0300

    Path to database connetion added.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,3 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+def get_db_connection():
+    conn = sqlite3.connect('database.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
